360e598fc35923447489bf6da84be6361fbf3974
unknown <>
2017-06-26 22:35:32 +0800

    index

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,19 +4,90 @@
             <el-col :span="16" class="left">
                 <el-row>
                     <el-col :span="24">
-                        <el-card class="box-card c_border_left">
+                        <el-card class="box-card c_border_left f1">
                             <div slot="header">
                                 <h3>订单统计</h3>
                             </div>
                             <el-col :span="24">
-                                <div class="charts_buttom" ref="charts_product"></div>
+                                <div class="charts_buttom" ref="order">
+                                    <ul class="order_box">
+                                        <li>
+                                            <el-row>
+                                                <header>今日统计</header>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>订单数</span>
+                                                    <span>{{order_data.day.num}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购数</span>
+                                                    <span>{{order_data.day.onum}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购金额</span>
+                                                    <span> <b v-if="order_data.day.ototal!=0">￥</b>{{order_data.day.ototal}}</span>
+                                                </el-col>
+                                            </el-row>
+                                        </li>
+                                        <li>
+                                            <el-row>
+                                                <header>本月统计</header>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>订单数</span>
+                                                    <span>{{order_data.month.num}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购数</span>
+                                                    <span>{{order_data.month.onum}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购金额</span>
+                                                    <span> <b v-if="order_data.month.ototal!=0">￥</b>{{order_data.month.ototal}}</span>
+                                                </el-col>
+                                            </el-row>
+                                        </li>
+                                        <li>
+                                            <el-row>
+                                                <header>本年统计</header>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>订单数</span>
+                                                    <span>{{order_data.year.num}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购数</span>
+                                                    <span>{{order_data.year.onum}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购金额</span>
+                                                    <span> <b v-if="order_data.year.ototal!=0">￥</b>{{order_data.year.ototal}}</span>
+                                                </el-col>
+                                            </el-row>
+                                        </li>
+                                        <li>
+                                            <el-row>
+                                                <header>总计</header>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>订单数</span>
+                                                    <span>{{order_data.total.num}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购数</span>
+                                                    <span>{{order_data.total.onum}}</span>
+                                                </el-col>
+                                                <el-col :span="8" class="order_lists">
+                                                    <span>采购金额</span>
+                                                    <span> <b v-if="order_data.total.ototal!=0">￥</b>{{order_data.total.ototal}}</span>
+                                                </el-col>
+                                            </el-row>
+                                        </li>
+                                    </ul>
+                                </div>
                             </el-col>
                         </el-card>
                     </el-col>
                 </el-row>
                 <el-row>
                     <el-col :span="24">
-                        <el-card class="box-card c_border_left">
+                        <el-card class="box-card c_border_left f2">
                             <div slot="header">
                                 <h3>采购金额报表</h3>
                             </div>
@@ -91,10 +162,16 @@ export default {
             charts_hospital: '', // 医院
             charts_product: '', // 产品
             charts_money: '', // 采购金额
-            chartLine_order: ''
+            order_data: {
+                day: {},
+                month: {},
+                year: {},
+                total: {}
+            } // 订单统计
         };
     },
     methods: {
+        // 处理饼图数据
         getCount (xdata) {
             let _ccount = [];
             _ccount.push({
@@ -112,7 +189,7 @@ export default {
             });
             return _ccount;
         },
-        // 获取
+        // 获取供应商/客户 数据
         init_charts_supplier_hospital () {
             this.Http.post(URL.SUPPLIER_HOSPITAL_COUNT, {
                 params: {}
@@ -126,6 +203,7 @@ export default {
                 this.setChart_pie(this.charts_hospital, name, hospitalData, '医院');
             });
         },
+        // 获取产品数据
         init_charts_product () {
             this.Http.post(URL.PRODUCT_COUNT, {
                 params: {}
@@ -139,6 +217,7 @@ export default {
                 this.setChart_pie(this.charts_product, chartsProductName, chartsProductData, '产品');
             });
         },
+         // 获取采购金额报表数据
         init_charts_money () {
             this.Http.post(URL.MONEY_COUNT, {
                 params: {}
@@ -152,12 +231,22 @@ export default {
                     ctotal.push(v.ctotal);
                     atotal.push(v.atotal);
                 }
-                datas = {ototal: ototal, ctotal: ctotal, atotal: atotal};
-                console.log(datas);
+                datas = { ototal: ototal, ctotal: ctotal, atotal: atotal };
+
                 this.setChart_line(this.charts_money, legendName, month, datas);
             });
         },
-
+        // 订单统计
+        async init_order () {
+            await this.Http.post(URL.ORDER_COUNT, {
+                params: {}
+            }).then(msg => {
+                for (let item in msg.data) {
+                    msg.data[item].ototal = parseMone(msg.data[item].ototal);
+                }
+                this.order_data = msg.data;
+            });
+        },
         // 饼形图方法
 
         setChart_pie (target, name, value, type) {
@@ -230,7 +319,7 @@ export default {
                 grid: {
                     left: '3%',
                     right: '4%',
-                    bottom: '15%',
+                    bottom: '18%',
                     containLabel: true
 
                 },
@@ -298,7 +387,9 @@ export default {
         }
 
     },
-
+    created () {
+        this.init_order();
+    },
     mounted () {
         this.init_charts_supplier_hospital();
         this.init_charts_product();
@@ -321,28 +412,73 @@ export default {
 </script>
 
 <style scoped lang="scss" rel="stylesheet/scss">
-$charts_bottom_left:670px;
+$charts_bottom_left:785px;
 $charts_bottom_right:440px;
 
 .contentwrapper {
-    background: #fff;
-    border: 1px solid #d1dbe5;
-    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .12), 0 0 6px 0 rgba(0, 0, 0, .04);
-    width: 100%;
+    // background: #fff;
+    // border: 1px solid #d1dbe5;
+    // box-shadow: 0 2px 4px 0 rgba(0, 0, 0, .12), 0 0 6px 0 rgba(0, 0, 0, .04);
+     width: 100%;
     .c_border_right {
         height: $charts_bottom_right;
-        margin: 10px; 
+        margin:0  0 10px 10px;
         .charts_buttom {
             padding: 10px;
             height: $charts_bottom_right - 40px;
         }
+    }  
+    .f1{
+        height: $charts_bottom_left - 200px;
+        .charts_buttom {
+             height: $charts_bottom_left - 240px;
+        }
     }
-     .c_border_left {
-        height: $charts_bottom_left;
-        margin: 10px; 
+    .f2{
+         height: $charts_bottom_left - 40px;
+         .charts_buttom {
+              height: $charts_bottom_left - 40px;
+        } 
+    }
+    .c_border_left {
+         margin:0  10px 10px 0;
         .charts_buttom {
             padding: 10px;
-            height: $charts_bottom_left - 40px;
+            .order_box {
+                height: 100%;
+                li {
+                    height: 24%;
+                    header {
+                        font-size: 14px;
+                        color: #527BDB;
+                        font-weight: bold;
+                        margin-bottom: 10px;
+                    }
+                    .order_lists {
+                        border: 1px solid #f0f0f0;
+                        border-right: none;
+                        height: 50px;
+                        line-height: 50px;
+                        &:nth-last-of-type(1) {
+                            border-right: 1px solid #f0f0f0;
+                        }
+                        padding:0 10px;
+                        display: flex;
+                        justify-content: space-between;
+                        span {
+                            &:nth-of-type(2) {
+                                font-weight: bold;
+                                color:#000;
+                            }
+                        }
+                    }
+                    &:hover {
+                        .order_lists {
+                            background: #f4f4f4;
+                        }
+                    }
+                }
+            }
         }
     }
 }
