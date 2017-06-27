a4720b3287504e43eebeb5686d2f5f2ba405ac09
yangjj <898718167@qq.com>
2017-06-27 17:36:54 +0800

    all

---
+++ src/components/dialog-table.vue
@@ -0,0 +1,59 @@
+/**分页插件*/
+<template>
+    <div class="ui-table"> 
+        <table width="100%" >
+            <tbody>
+                <tr v-for="(item,index) in datas ">
+                    <td :class="item.id" class="table_name">{{item.name}}</td>
+                    <td>{{item.text}}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+<script>
+export default {
+    name: 'dailog-table',
+    data () {
+        return {
+           
+        };
+    },
+  // 需要传的参数
+    props: {
+        datas: { // 数据
+            type: [String, Array],
+            required: true
+        }
+    }
+};
+</script>
+
+<style lang="scss" scoped rel="stylesheet/scss">
+.ui-table{
+    table {
+        border-left: 1px solid #ddd;
+        border-bottom: 1px solid #ddd;
+        td {
+            border-right: 1px solid #ddd;
+            border-top: 1px solid #ddd;
+            color: #333;
+            line-height: 24px;
+            padding: 5px;
+            word-break: break-all;
+            word-wrap: break-word;
+        }
+        .table_name{
+            width: 90px;
+            background-color: #f9f9f9;
+            text-align: right;
+            padding: 0 10px 0 0;
+            color: #666;
+            text-indent: 0;
+            vertical-align: middle;
+        }
+    }
+}
+
+ 
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
 ﻿const dashboard = r => require.ensure([], () => r(require('../pages/dashboard/Dashboard')), 'dashboard');
-const charts = r => require.ensure([], () => r(require('../pages/echarts/echarts')), 'charts');
-const charts = r => require.ensure([], () => r(require('../pages/echarts/echarts')), 'charts');
 const supplierChart = r => require.ensure([], () => r(require('../pages/chart/supplier/SupplierChart')), 'chart');
 const hospitalChart = r => require.ensure([], () => r(require('../pages/chart/hospital/HospitalChart')), 'chart');
 const user = r => require.ensure([], () => r(require('../pages/user/User')), 'user');
@@ -20,11 +18,9 @@ export default [
         name: 'dashboard',
         component: dashboard,
         children: [
-            { path: '/index', component: charts, name: 'charts' }
-            { path: '/index', component: charts, name: 'charts' },
             { path: '/supplierChart', component: supplierChart, name: 'supplierChart' },
             { path: '/hospitalChart', component: hospitalChart, name: 'hospitalChart' },
-            { path: '/user', component: user, name: 'user' }
+            { path: '/user', component: user, name: 'user' },
             { path: '/index', component: index, name: 'index' },
             { path: '/supplier', component: supplier, name: 'supplier' }
         ]
